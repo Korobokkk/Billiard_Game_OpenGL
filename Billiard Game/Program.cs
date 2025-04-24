95b11c89726752740de487540df9e589c82746af
Korobokkk <bbb754748@gmail.com>
2025-04-24 21:30:04 +0300

    added Class Camera.cs, rotation Cube and microfix

--- Billiard Game/Program.cs
+++ Billiard Game/Program.cs
@@ -5,6 +5,7 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
+namespace Open_TK;
 class Program
 {
     static void Main(string[] args)
